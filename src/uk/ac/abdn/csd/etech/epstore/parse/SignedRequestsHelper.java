bb1c38e846e353ce3d5a360a2f4d2d592c79acd4
unknown <bscharla@.staff.csd.abdn.ac.uk>
2010-08-05 13:51:10 +0100

    tidied up files

--- src/uk/ac/abdn/csd/etech/epstore/parse/SignedRequestsHelper.java
+++ src/uk/ac/abdn/csd/etech/epstore/parse/SignedRequestsHelper.java
@@ -41,8 +41,8 @@ public class SignedRequestsHelper {
   private String endpoint = "xml-uk.amznxslt.com"; // must be lowercase
  
   // change this so reads from properties file
-  private String awsAccessKeyId = "1QHTGHD6D2FH3K1XC382";
-  private String awsSecretKey = "HE9iMl9Mv5iaEgiWDvn6ZX6Vdvn8jNmRLyqdNN03";
+  private String awsAccessKeyId = "my accese key ID";
+  private String awsSecretKey = "my secret access key";
 
   private SecretKeySpec secretKeySpec = null;
   private Mac mac = null;
